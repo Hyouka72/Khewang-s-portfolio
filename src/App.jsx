546e68de38d78c5302d02d149ea3993539f3ade5
Hyouka45 <khewang212@gmail.com>
2025-12-26 14:02:55 +0545

    remaining

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,44 @@
 import React, { useEffect, useState } from 'react'
 import NavBar from './components/NavBar'
 import Home from './components/Home'
-import SocialLink from './components/SocialLink';
-import About from './components/About';
-import Portfolio from './components/Portfolio';
-import Experience from './components/Experience';
-import Contact from './components/Contact';
-
-import Loder from './components/Loder';
+import SocialLink from './components/SocialLink'
+import About from './components/About'
+import Portfolio from './components/Portfolio'
+import Experience from './components/Experience'
+import Contact from './components/Contact'
+import Loder from './components/Loder'
 
 const App = () => {
 
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
+    const handleLoad = () => {
+      setLoading(false)
+    }
 
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 3000);
+    window.addEventListener('load', handleLoad)
 
     return () => {
-      
-      clearTimeout(timer);
-    };
-  }, []);
-
+      window.removeEventListener('load', handleLoad)
+    }
+  }, [])
 
   return (
     <div>
-      {
-        loading ? (
-           <Loder loading={loading} />
-        ) : (
-          <>
-            <NavBar />
-            <SocialLink />
-            <Home />
-            <About />
-            <Portfolio />
-            <Experience />
-            <Contact />
-          </>
-        )
-      }
+      {loading ? (
+        <Loder />
+      ) : (
+        <>
+          <NavBar />
+          <SocialLink />
+          <Home />
+          <About />
+          <Portfolio />
+          <Experience />
+          <Contact />
+        </>
+      )}
     </div>
   )
 }
